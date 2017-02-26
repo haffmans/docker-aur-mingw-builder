a12b4ca6060a1a36a16004f5e9c5da777f1e162c
Wouter Haffmans <wouter@simply-life.net>
2017-02-26 16:37:52 +0100

    Solve cyclic dependencies
    The package list now has a list of forced packages which are added to
    the beginning of the list. A bit of syntax allows to force package
    installation etc.
    
    This way packages like freetype2 and harfbuzz are installed in a
    specific order, such that we shouldn't end up in an endless loop in
    yaourt anymore.

--- update-packages.sh
+++ update-packages.sh
@@ -1,4 +1,66 @@
-#!/bin/sh
+#!/bin/bash
 
-echo mingw-w64-freetype2-bootstrap > packages.txt
-yaourt -Ssqm mingw-w64 >> packages.txt
+# Early install package list (to fix cyclic dependencies) and some real basic common packages
+# Prefix with '*' to force install (ignore conflicts; Pacman --force option),
+#             '^' to ignore dependencies (Pacman --nodeps option) and
+#             '!' to uninstall the package
+preinstall=(
+    mingw-w64-configure
+    mingw-w64-bzip2
+    mingw-w64-zlib
+
+    mingw-w64-freetype2-bootstrap
+    mingw-w64-cairo-bootstrap
+    mingw-w64-glib2
+    mingw-w64-harfbuzz
+    *mingw-w64-freetype2
+    *mingw-w64-cairo
+    *mingw-w64-glib2
+    *mingw-w64-harfbuzz
+
+    mingw-w64-x264-bootstrap
+    mingw-w64-ffmpeg
+    mingw-w64-x264
+)
+
+exclude=(
+    -git$
+    -git-vslib$
+    -svn$
+    -samples$
+
+    ^mingw-w64-python-bin$
+    ^mingw-w64-python2-bin$
+    ^mingw-w64-python26-bin$
+    ^mingw-w64-python33-bin$
+    ^mingw-w64-python34-bin$
+    ^mingw-w64-python35-bin$
+)
+
+function sanitize_name() {
+    echo $1 | sed 's/\(!\|^[*\^]\{1,2\}\)//'
+}
+
+# First inject early install packages (and exclude them further on)
+truncate -s 0 packages.txt
+for package in ${preinstall[@]}; do
+    echo $package >> packages.txt
+    excludepackage=$(sanitize_name ${package})
+    exclude=( ${exclude[@]} "^${excludepackage}$" )
+done
+
+# Get list of mingw-w64 packages and write them in packages.txt
+yaourt -Ssqm mingw-w64 | while read package; do
+    excl=0
+    for pattern in ${exclude[@]}; do
+        if [[ ${package} =~ ${pattern} ]]; then
+            excl=1
+        fi
+    done
+
+    if [[ $excl -gt 0 ]]; then
+        continue;
+    fi
+
+    echo $package >> packages.txt
+done
--- update.sh
+++ update.sh
@@ -1,7 +1,43 @@
 #!/bin/sh
 
+function sanitize_name() {
+    echo $1 | sed 's/\(!\|^[*\^]\{1,2\}\)//'
+}
+
 while read pkg; do
-    yaourt -S --needed --aur $pkg
+    params=""
+    doyes="0"
+    environment=""
+    # '!' means remove
+    if [[ "$pkg" =~ ^\! ]]; then
+        echo "Removing $(sanitize_name $pkg)!"
+        params="-R"
+    else
+        echo "Installing $(sanitize_name $pkg)..."
+        params="-S --needed --aur"
+    fi
+
+    # '*' means force install
+    if [[ "$pkg" =~ ^\^?\* ]]; then
+        echo "Forcing $(sanitize_name $pkg)!"
+        # Also remove 'needed' option; add --confirm to make sure we can explicitly answer 'yes' to conflict resolutions
+        params="${params//--needed/} --force --confirm"
+        doyes="1"
+        environment="NOCONFIRM=0 PU_NOCONFIRM=0"
+    fi
+    # '^' means --nodeps
+    if [[ "$pkg" =~ ^\*?\^ ]]; then
+        echo "Ignoring dependencies for $(sanitize_name $pkg)!"
+        params="$params --nodeps"
+    fi
+
+    pkg=${pkg#[\\!\*]}
+    if [[ "$doyes" == "1" ]]; then
+        echo "Answering yes to all Pacman confirmations!"
+        yes | env ${environment} yaourt $params $pkg
+    else
+        env ${environment} yaourt $params $pkg
+    fi
 done < packages.txt
 
 cd /build/repo
